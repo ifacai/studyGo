092217c03e13ed41f3ddda41ecf8721a850de93a
facai <madebyphp@outlook.com>
2022-11-06 11:21:37 +0800

    Add files via upload

---
+++ 基础/数组/数组取值.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int{1, 2, 3}
+	fmt.Println(a[0])   //结果为1
+	fmt.Println(a[1])   // 结果2
+	fmt.Println(a[2])   // 结果3
+	fmt.Println(len(a)) // 数组长度
+
+}
