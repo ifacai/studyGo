9ce9702cfe516741d8314d230ca0c13d7fe2d86b
facai <madebyphp@outlook.com>
2022-11-12 13:41:34 +0800

    Add files via upload

---
+++ 基础/切片/切片初始化.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+var arr = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+var slice0 []int = arr[2:8]
+var slice1 []int = arr[0:6]        //可以简写为 var slice []int = arr[:end]
+var slice2 []int = arr[5:10]       //可以简写为 var slice[]int = arr[start:]
+var slice3 []int = arr[0:len(arr)] //var slice []int = arr[:]
+var slice4 = arr[:len(arr)-1]      //去掉切片的最后一个元素
+func main() {
+	fmt.Printf("全局变量：arr %v\n", arr)
+	fmt.Printf("全局变量：slice0 %v\n", slice0)
+	fmt.Printf("全局变量：slice1 %v\n", slice1)
+	fmt.Printf("全局变量：slice2 %v\n", slice2)
+	fmt.Printf("全局变量：slice3 %v\n", slice3)
+	fmt.Printf("全局变量：slice4 %v\n", slice4)
+	fmt.Printf("-----------------------------------\n")
+	arr2 := [...]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	slice5 := arr[2:8]
+	slice6 := arr[0:6]         //可以简写为 slice := arr[:end]
+	slice7 := arr[5:10]        //可以简写为 slice := arr[start:]
+	slice8 := arr[0:len(arr)]  //slice := arr[:]
+	slice9 := arr[:len(arr)-1] //去掉切片的最后一个元素
+	fmt.Printf("局部变量： arr2 %v\n", arr2)
+	fmt.Printf("局部变量： slice5 %v\n", slice5)
+	fmt.Printf("局部变量： slice6 %v\n", slice6)
+	fmt.Printf("局部变量： slice7 %v\n", slice7)
+	fmt.Printf("局部变量： slice8 %v\n", slice8)
+	fmt.Printf("局部变量： slice9 %v\n", slice9)
+}
