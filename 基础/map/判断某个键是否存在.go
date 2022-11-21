13896be369af1f36c42aacc358f8c0db4c2bc3ee
facai <madebyphp@outlook.com>
2022-11-21 10:27:38 +0800

    Create 判断某个键是否存在.go

---
+++ 基础/map/判断某个键是否存在.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//    value, ok := map[key]
+
+	scoreMap := make(map[string]int)
+	scoreMap["张三"] = 90
+	scoreMap["小明"] = 100
+	// 如果key存在ok为true,v为对应的值；不存在ok为false,v为值类型的零值
+	v, ok := scoreMap["张三"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("查无此人")
+	}
+}
