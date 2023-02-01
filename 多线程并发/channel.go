a7de04110a8bfe317ffd77860324284496a21b1b
facai <madebyphp@outlook.com>
2023-02-01 13:46:02 +0800

    Create channel.go

---
+++ 多线程并发/channel.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func showMsg(i int) {
+	//wg.Done == wg.Add(-1)
+	defer wg.Done()
+	//defer 延迟执行 最后执行
+	fmt.Println("第  ", i, "  次")
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		go showMsg(i)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println("主进程.end")
+}
