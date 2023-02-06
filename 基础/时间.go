29e4f1c3fef576af73e7c682ed314974f8386c49
facai <madebyphp@outlook.com>
2023-02-06 09:39:43 +0800

    Update 时间.go

--- 基础/时间.go
+++ 基础/时间.go
@@ -13,3 +13,11 @@ func nowTime() {
 	fmt.Printf("类型:\n%T\n值:\n%v\n", timestamp, timestamp)
 
 }
+
+func stringToUnixTime(inputStringTime string) string {
+	timeLayout := "2006-01-02 15:04:05"
+	loc, _ := time.LoadLocation("Local")
+	theTime, _ := time.ParseInLocation(timeLayout, inputStringTime, loc)
+	timeUnix := theTime.Unix()
+	return strconv.FormatInt(timeUnix, 10)
+}
