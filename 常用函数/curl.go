df7f242158e5b3b621d7116819baa313016d584a
facai <madebyphp@outlook.com>
2023-02-08 15:03:05 +0800

    Update curl.go

--- 常用函数/curl.go
+++ 常用函数/curl.go
@@ -56,3 +56,56 @@ func curlGet(inputUrl, cookie string) string {
 	res, err := io.ReadAll(body)
 	return string(res)
 }
+
+func curlGetProxySOCKS5(inputUrl string) string {
+	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:7890", nil, proxy.Direct)
+	if err != nil {
+		fmt.Println(err, "can't connect to the proxy:", err)
+		os.Exit(1)
+	}
+	dialContext := func(ctx context.Context, network, address string) (net.Conn, error) {
+		// do anything with ctx
+		return dialer.Dial(network, address)
+	}
+	tr := &http.Transport{
+		DialContext: dialContext,
+	}
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Second * 60,
+	}
+	request, err := http.NewRequest("GET", inputUrl, nil)
+	request.Header.Add("Accept-Language", "zh-Hans-CN;q=1")
+	request.Header.Add("Accept-Encoding", "gzip")
+	request.Header.Add("Connection", "close")
+	request.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44")
+	if err != nil {
+		return ""
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("response err")
+		fmt.Println(err)
+		return ""
+	}
+	//defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(response.Body)
+	var body io.Reader
+	switch response.Header.Get("Content-Encoding") {
+	case "gzip":
+		body, err = gzip.NewReader(response.Body)
+		break
+	case "deflate":
+		body = flate.NewReader(response.Body)
+		break
+	default:
+		body = response.Body
+	}
+	res, err := io.ReadAll(body)
+	return string(res)
+}
